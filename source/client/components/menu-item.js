a3e84e67b33cf7bab47b5e529efa4586d99c4f00
Florian Wendelborn <dodekeract@users.noreply.github.com>
2017-02-03 01:02:25 +0100

    Better Placeholder

--- source/client/components/menu-item.js
+++ source/client/components/menu-item.js
@@ -8,7 +8,7 @@ import {html} from 'hyperapp'
 
 export default ({name, description, add}) => html`
 	<div class="card">
-		<img class="card-img-top" src="http://lorempizza.com/380/240" alt="Card image cap"/>
+		<img class="card-img-top" src="http://lorempizza.com/380/240/${name}" alt="Card image cap"/>
 		<div class="card-block">
 			<h4 class="card-title">${name}</h4>
 			<p class="card-text">${description}</p>
